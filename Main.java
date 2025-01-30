f6db8a3b11d6083caa33635bf581bb77515edd76
Sebastian Westin Ingebrigtsen <sebasti@stud.ntnu.no>
2025-01-30 20:41:38 +0100

    clean

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Opprett et Student-objekt
-        Student student1 = new Student("Bob", 21, "Computer Science");
-
-        // Vis informasjon
-        student1.displayInfo();
-    }
-}
